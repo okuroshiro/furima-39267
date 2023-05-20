9b9fbeaa9a28ecdd5a5a9bd3b2d8f3db4026c3af
okuroshiro <lrrkfeamhc14whale@gmail.com>
2023-05-20 11:53:38 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  
+
+  validates :name,        presence: true
+  validates :birth_day,   presence: true
+  validates :family_name, presence: true
+  validates :first_name,  presence: true
+  validates :family_name_reading,   presence: true
+  validates :first_name_reading,    presence: true
 end
