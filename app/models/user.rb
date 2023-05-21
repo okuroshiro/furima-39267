8b566e1dcf5617c06b6cd3bd0a11a33fb826a9b7
okuroshiro <lrrkfeamhc14whale@gmail.com>
2023-05-21 14:24:23 +0900

    新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@ class User < ApplicationRecord
 
   validates :name,        presence: true
   validates :birth_day,   presence: true
-  validates :family_name, presence: true
-  validates :first_name,  presence: true
-  validates :family_name_reading,   presence: true
-  validates :first_name_reading,    presence: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' is invalid. Input full-width characters' }
+  validates :first_name,  presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' is invalid. Input full-width characters' }
+  validates :family_name_reading,   presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: ' is invalid. Input full-width katakana characters' }
+  validates :first_name_reading,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: ' is invalid. Input full-width katakana characters' }
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: ' is invalid. Include both letters and numbers' }
 end
