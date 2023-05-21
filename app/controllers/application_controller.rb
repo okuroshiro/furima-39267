70ffc91ce4274afa6064d41c7bd3bfce7b8ef464
okuroshiro <lrrkfeamhc14whale@gmail.com>
2023-05-21 14:55:18 +0900

    Rubocop実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birth_day, :family_name, :first_name, :family_name_reading, :first_name_reading])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:name, :birth_day, :family_name, :first_name, :family_name_reading,
+                                             :first_name_reading])
   end
 end
