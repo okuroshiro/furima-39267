58e190bf0899d58e31f4a8308a9f52694dfb0d17
okuroshiro <lrrkfeamhc14whale@gmail.com>
2023-05-23 21:08:07 +0900

    Active_hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :payer
+  belongs_to :prefecture
+  belongs_to :duration
+
   validates :item_name, presence: true 
   validates :content, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :payer_id, presence: true 
-  validates :prefecture_id, presence: true
-  validates :duration_id, presence: true
+  validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :payer_id, numericality: { other_than: 0, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :duration_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :price, presence: true
   validates :image, presence: true
 end
