c5735ea7a09bae285fe78607b326f2b87b685e30
okuroshiro <lrrkfeamhc14whale@gmail.com>
2023-05-26 10:40:25 +0900

    価格の計算機能

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("./item_price");
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
