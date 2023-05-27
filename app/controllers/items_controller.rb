a091aef48314b3c5b32e8dfc8747d468a760eb3c
okuroshiro <lrrkfeamhc14whale@gmail.com>
2023-05-27 17:26:51 +0900

    rubocopによる整形

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,8 +45,8 @@ class ItemsController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:id])
-    unless user_signed_in? && current_user == @item.user
-      redirect_to root_path
-    end
+    return if user_signed_in? && current_user == @item.user
+
+    redirect_to root_path
   end
 end
