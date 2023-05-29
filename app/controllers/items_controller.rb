2beb807f5ea9bd25f0787f9d8ea07fae62abbb56
okuroshiro <lrrkfeamhc14whale@gmail.com>
2023-05-29 11:06:57 +0900

    move_to_indexの記述変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,9 +52,9 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    return if user_signed_in? && current_user == @item.user
-
-    redirect_to root_path
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user == @item.user
+      redirect_to root_path
+    end
   end
-
 end
