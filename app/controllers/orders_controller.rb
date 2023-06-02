02427760003faac6f48d5b75504b0416bfc12355
okuroshiro <lrrkfeamhc14whale@gmail.com>
2023-06-02 11:06:21 +0900

    コントローラーの設定

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,47 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!,  except: [:index]
+  before_action :move_to_index, only: [:index]
+
+  def index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :tel, :token, item: [:price]).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+    end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    unless user_signed_in? && current_user == @item.user && @item.order.nil?
+      if user_signed_in?
+        redirect_to root_path
+      else
+        redirect_to new_user_session_path
+      end
+    end
+  end
+end
