d904bbc754073e0d4e8323c0e93090a0300502d4
okuroshiro <lrrkfeamhc14whale@gmail.com>
2023-06-03 12:51:15 +0900

    不要な記述の削除

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -13,18 +13,14 @@ class OrderAddress
     validates :item_id
   end
 
-  validates :tel, length: { minimum: 10, message: 'is too short' }
+  validates :tel, length: { minimum: 10, maximum: 11, message: 'is too short' }
   validates :tel, format: { with: /\A\d+\z/, message: 'is invalid. Input only numbers' }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id, token: token)
-    if order.valid?
-      order.save
+   
       Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number,
                      building_name: building_name, tel: tel, order_id: order.id)
-    else
-      false
-    end
   end
 
   def user
