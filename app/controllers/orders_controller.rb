a8b8139189b53ff14a20e0fff8a365f6daf103c5
okuroshiro <lrrkfeamhc14whale@gmail.com>
2023-06-04 15:17:27 +0900

    保存に関する修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,7 @@ class OrdersController < ApplicationController
   end
 
   def create
+
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -38,8 +39,9 @@ class OrdersController < ApplicationController
   def set_item
     @item = Item.find(params[:item_id])
   end
-end
 
-def move_to_index
-  redirect_to root_path if current_user.id == @item.user.id || @item.order.present?
+  def move_to_index
+    redirect_to root_path if current_user.id == @item.user.id || @item.order.present?
+  end
+
 end
